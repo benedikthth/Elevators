11f02e0dd897168411f5cb0b3b9115b3763228ad
Benedikt Hólm Þórðarson <benedikthth@gmail.com>
2018-01-23 21:17:00 +0000

    Initial Commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+
+	public GameObject tryAgainButton;
+
+	public Camera mainCamera;
+	public GameObject axe;
+	public Rigidbody2D axebody;
+	public float originalOrtographicSize;
+	public float vel;
+
+	public GameObject ground;
+	public Sprite groundSprite;
+
+	public float zoom = 0;
+
+	private bool thrown;
+
+	void axeThrowEvent(){
+		thrown = true;
+	}
+
+	void tryAgain(){
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+	}
+
+	void axeLandEvent(){
+		thrown = false;
+		tryAgainButton.SetActive (true);
+	}
+
+	// Use this for initialization
+	void Start () {
+		originalOrtographicSize = mainCamera.orthographicSize;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		zoom -= Input.mouseScrollDelta.y;
+
+
+		float groundOffset = mainCamera.transform.position.x % groundSprite.bounds.size.x; 
+
+		ground.transform.position = new Vector3 (
+											     mainCamera.transform.position.x - groundOffset,
+												 ground.transform.position.y,
+												 ground.transform.position.z
+												);
+		float axeModifier = 0;
+		if (thrown) {
+
+			vel = axebody.velocity.magnitude;
+			mainCamera.transform.position = axe.transform.position + new Vector3 (0, 0, -10);
+			axeModifier = (3 * axebody.velocity.magnitude * .2f);
+		}
+
+		mainCamera.orthographicSize = originalOrtographicSize + zoom + axeModifier;
+	
+	}
+}
---
+++ Assets/Scripts/HeadController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadController : MonoBehaviour {
+
+	public GameObject axe;
+	SpriteRenderer sr;
+	public float angle;
+
+	// Use this for initialization
+	void Start () {
+		sr = gameObject.GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 dir =  transform.position - axe.transform.position;
+
+		angle = Mathf.Atan2 (dir.y, dir.x) * Mathf.Rad2Deg;
+
+		if (angle > 90 || angle < -90) {
+			sr.flipY = true;
+		} else {
+			sr.flipY = false;
+		}
+
+		transform.rotation = Quaternion.AngleAxis (angle, Vector3.forward);
+	}
+}
